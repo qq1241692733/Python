d7409773f48667ad700a71a9b6fa1608f4a143d2
qq1241692733 <1241692733@qq.com>
2021-08-11 21:59:58 +0800

    自动化测试

--- selenium/test0805/test07.py
+++ selenium/test0805/test07.py
@@ -1,12 +1,17 @@
 from selenium import webdriver
 import time
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.keys import Keys                    # 键盘
+from selenium.webdriver.common.action_chains import ActionChains   # 鼠标
 
 driver = webdriver.Chrome()
 driver.get("https://www.baidu.com/")
 
 driver.find_element_by_xpath("//*[@id='kw']").send_keys("天气之子")
-driver.find_element_by_xpath("//*[@id='su']").click()
+# 鼠标
+q = driver.find_element_by_xpath("//*[@id='su']")
+ActionChains(driver).double_click(q).perform()  # 双击
+time.sleep(4)
+q = driver.find_element_by_xpath("//*[@id='1']/h3/a")
+ActionChains(driver).context_click(q).perform()  # 右击
 time.sleep(4)
 driver.quit()
